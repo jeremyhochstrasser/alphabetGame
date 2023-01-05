f60d06c0fbc42100fc9bc68aa4bf5108aeae1ccc
Jeremy Hochstrasser <hochstrasserjeremy@gmail.com>
2023-01-05 01:07:27 -0700

    adds minimal functionality and css

--- alpha_game/src/App.js
+++ alpha_game/src/App.js
@@ -3,22 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="center">
+      <h1>Rock Paper Scissors</h1>
+      <div>
+        <div className="container">
+          <div className="player">
+            <div>Player 1: 0</div>
+            <div></div>
+          </div>
+          <div className="player">
+            <div>Player 2: 0</div>
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <button className="round-btn">Rock</button>
+          <button className="round-btn">Paper</button>
+          <button className="round-btn">Scissors</button>
+        </div>
+        </div>
+        <h1>Player 1 Wins</h1>
+      </div>
   );
 }
 
